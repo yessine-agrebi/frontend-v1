8f49df559bcf067769d24c133c78ef11cad659b7
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-03-05 21:18:26 +0100

    create navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,79 +1,56 @@
 "use client";
 import React, { useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
-import { AiOutlineClose } from "react-icons/ai";
-import Link from "next/link";
+import { Button } from "./ui/button";
+import { ModeToggle } from "./ModeToggle";
+import { Menu } from "lucide-react";
+import { DropdownMenu, DropdownMenuShortcut } from "./ui/dropdown-menu";
+import {
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const navLinks = [
-    { href: "/", label: "Home" },
-    { href: "/tutors", label: "Find Tutors" },
-    { href: "/tutor/register", label: "Become a Tutor" },
-    { href: "/contact", label: "Contact Us" },
-  ];
+  const [isMobile, setIsMobile] = useState(false);
+
   return (
-    <>
-      <header className="sm:px-8 px-4 py-2 z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
-          <Link href="/" className="text-3xl font-bold">
-            <h1>TutorZone</h1>
-          </Link>
-          <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-            {navLinks.map((item) => (
-              <li key={item.label}>
-                <Link
-                  href={item.href}
-                  className="font-montserrat leading-normal text-lg text-slate-gray"
-                >
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
-            <Link href="/user/login">
-              <button className="bg-yellow-300 hover:bg-yellow-200 text-black py-2 px-4 rounded-full border-2 border-black">
-                Sign In
-              </button>
-            </Link>
-          </div>
-          <div
-            className="hidden max-lg:block cursor-pointer"
-            onClick={() => {
-              setIsMenuOpen(!isMenuOpen);
-            }}
-          >
-            <RxHamburgerMenu className="text-4xl" />
-          </div>
-        </nav>
-      </header>
-      {isMenuOpen && (
-        <div>
-          <nav className="fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto bg-slate-100  ">
-            <div
-              className="hidden max-lg:block fixed right-0  px-8 py-4 cursor-pointer"
-              onClick={() => {
-                setIsMenuOpen(!isMenuOpen);
-              }}
-            >
-              <AiOutlineClose className="text-4xl" />
-            </div>
-            <ul className=" lg:hidden flex flex-col items-center justify-center h-full ">
-              {navLinks.map((item) => (
-                <li key={item.label}>
-                  <a
-                    href={item.href}
-                    className="font-montserrat leading-normal text-lg text-slate-gray"
-                  >
-                    {item.label}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      )}
-    </>
+    <nav className="flex py-4 justify-between md:shadow-sm">
+      <div className="flex items-center gap-4">
+        <h1 className="font-extrabold text-2xl">9arrini</h1>
+
+        <ul className="md:flex hidden items-start justify-center gap-3 ">
+          <li>Find Tutors</li>
+          <li>Become a Tutor</li>
+        </ul>
+      </div>
+      <div className="md:flex hidden items-center gap-4">
+        <ModeToggle />
+        <Button>Login</Button>
+      </div>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button size="icon">
+              <Menu />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-50 mx-3 flex-col items-center justify-center">
+            <DropdownMenuLabel>Login</DropdownMenuLabel>
+            <DropdownMenuSeparator className="bg-gray-300 my-1 h-[0.5px]" />
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Home</DropdownMenuItem>
+              <DropdownMenuItem>Find Tutors</DropdownMenuItem>
+              <DropdownMenuItem>Become a Tutor</DropdownMenuItem>
+              <DropdownMenuItem>Messages</DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
   );
 };
+
 export default Navbar;
