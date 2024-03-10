42520c5714767c0ca546776a5317e9f602edd346
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-03-10 20:39:38 +0100

    create tutor page

---
+++ app/tutors/[tutorId]/page.tsx
@@ -0,0 +1,13 @@
+import { useParams } from 'next/navigation'
+import React from 'react'
+
+
+
+const Tutor = () => {
+    const tutorId = useParams()
+  return (
+    <div>Tutor</div>
+  )
+}
+
+export default Tutor
