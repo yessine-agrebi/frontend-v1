a1f56424054301d73f744413f21feddaa48d62cb
yessine-agrebi <yessine.agrebi.aiesec@gmail.com>
2024-03-14 22:28:36 +0100

    formatting the code

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { Availability } from '@/types';
+import React, { useState } from "react";
+import { Availability } from "@/types";
 
 const Calendar = ({ availabilities }: { availabilities: Availability[] }) => {
-  console.log(availabilities)
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  console.log(availabilities);
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   const [currentDate, setCurrentDate] = useState(new Date());
 
@@ -17,10 +17,14 @@ const Calendar = ({ availabilities }: { availabilities: Availability[] }) => {
 
   // Filter availabilities for the current week
   const firstDayOfWeek = new Date(currentDate);
-  const lastDayOfWeek = new Date(currentDate.setDate(currentDate.getDate() + 6));
+  const lastDayOfWeek = new Date(
+    currentDate.setDate(currentDate.getDate() + 6)
+  );
   const availabilitiesThisWeek = availabilities.filter((availability) => {
     const availabilityDate = new Date(availability.startDate);
-    return availabilityDate >= firstDayOfWeek && availabilityDate <= lastDayOfWeek;
+    return (
+      availabilityDate >= firstDayOfWeek && availabilityDate <= lastDayOfWeek
+    );
   });
 
   // Generate an array of 7 dates representing the days of the current week
@@ -38,7 +42,12 @@ const Calendar = ({ availabilities }: { availabilities: Availability[] }) => {
     let currentTime = start;
 
     while (currentTime <= end) {
-      timeSlots.push(currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+      timeSlots.push(
+        currentTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        })
+      );
       currentTime.setMinutes(currentTime.getMinutes() + 30);
     }
 
@@ -53,20 +62,27 @@ const Calendar = ({ availabilities }: { availabilities: Availability[] }) => {
       </div>
       <div className="grid grid-cols-7 border-b-2 border-yellow-300">
         {daysOfWeek.map((day, index) => (
-          <div key={index} className="p-2 text-center text-gray-400">{day}</div>
+          <div key={index} className="p-2 text-center text-gray-400">
+            {day}
+          </div>
         ))}
       </div>
       <div className="grid grid-cols-7 gap-2">
         {daysOfMonth.map((day, index) => (
           <div key={index} className="p-2 text-center">
-            <div className='text-gray-400'>{day.getDate()}</div> 
+            <div className="text-gray-400">{day.getDate()}</div>
             {availabilitiesThisWeek.map((availability, i) => {
-              const timeSlots = generateTimeSlots(availability.startTime, availability.endTime);
-              console.log(timeSlots)
+              const timeSlots = generateTimeSlots(
+                availability.startTime,
+                availability.endTime
+              );
+              console.log(timeSlots);
               return (
                 <React.Fragment key={i}>
                   {timeSlots.map((time, j) => (
-                    <div key={j} className="py-2 font-semibold">{time}</div> 
+                    <div key={j} className="py-2 font-semibold">
+                      {time}
+                    </div>
                   ))}
                 </React.Fragment>
               );
